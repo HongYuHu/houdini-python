509f50e25abe94f30fa925b07711eb32a4833501
HongYuHu <84280874+HongYuHu@users.noreply.github.com>
2021-05-17 01:59:13 +0800

    Update createMaterial.py

--- createMaterial.py
+++ createMaterial.py
@@ -1,9 +1,15 @@
 def createTexture():
-    mat = hou.node("/mat")
-    matnode = mat.createNode("redshift_vopnet","wood_mat")
-    txBasecolor = matnode.createNode("redshift::TextureSampler","tx_basecolor")
+    obj = hou.node("/obj")
+    matnet = obj.createNode("matnet","matnet",exact_type_name = matnet)
+    matnode = matnet.createNode("redshift_vopnet","wood_mat",exact_type_name=1)
+    #childen = matnode.childen()
+    #childen.destroy()
+
+    txBasecolor = matnode.createNode("redshift::TextureSampler","tx_basecolor")    
     txcolorcorrect =matnode.createNode("redshift::RSColorCorrection","colorCorrection")
-    material = matnode.createNode("redshift::Material","Material")
+    material = matnode.createNode("redshift::Material","Material1")
+    shader = matnode.createNode("redshift_material","redshift_material1")
+    shader.setInput(0,material,0)
     material.setInput(1,txBasecolor,0)
     #nodename = node.name()
     
